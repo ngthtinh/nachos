c08148790716ae633f351eed3c11522abe97a3ba
ngthtinh <nguyenthanhtinh365@gmail.com>
2022-03-11 10:16:25 +0700

    Patch

--- NachOS-4.0/code/userprog/syscall.h
+++ NachOS-4.0/code/userprog/syscall.h
@@ -18,24 +18,33 @@
 /* system call codes -- used by the stubs to tell the kernel which system call
  * is being asked for
  */
-#define SC_Halt		0
-#define SC_Exit		1
-#define SC_Exec		2
-#define SC_Join		3
-#define SC_Create	4
+#define SC_Halt		    0
+#define SC_Exit		    1
+#define SC_Exec		    2
+#define SC_Join		    3
+#define SC_Create	    4
 #define SC_Remove       5
-#define SC_Open		6
-#define SC_Read		7
-#define SC_Write	8
+#define SC_Open		    6
+#define SC_Read		    7
+#define SC_Write	    8
 #define SC_Seek         9
-#define SC_Close	10
+#define SC_Close	    10
 #define SC_ThreadFork	11
 #define SC_ThreadYield	12
-#define SC_ExecV	13
+#define SC_ExecV	    13
 #define SC_ThreadExit   14
 #define SC_ThreadJoin   15
 
-#define SC_Add		42
+#define SC_Add		    42
+
+// Define system call code
+#define SC_ReadNum      50
+#define SC_PrintNum     51
+#define SC_ReadChar     52
+#define SC_PrintChar    53
+#define SC_RandomNum    54
+#define SC_ReadString   55
+#define SC_PrintString  56
 
 #ifndef IN_ASM
 
@@ -58,6 +67,55 @@ void Halt();
  */ 
 
 int Add(int op1, int op2);
+
+/*
+ * System call: ReadNum
+ * Input : None
+ * Output: int - read integer
+ */
+int ReadNum();
+
+/*
+ * System call: PrintNum
+ * Input : int - an integer to printed
+ * Output: None
+ */
+void PrintNum(int number);
+
+/*
+ * System call: ReadChar
+ * Input : None
+ * Output: char - read character
+ */
+char ReadChar();
+
+/*
+ * System call: PrintChar
+ * Input : char - character to printed
+ * Output: None
+ */
+void PrintChar(char character);
+
+/*
+ * System call: RandomNum
+ * Input : None
+ * Output: int - random integer
+ */
+int RandomNum();
+
+/*
+ * System call: ReadString
+ * Input : char[] - buffer, int - length of string
+ * Output: None
+ */
+void ReadString(char buffer[], int length);
+
+/*
+ * System call: PrintString
+ * Input : char[] - buffer
+ * Output: None
+ */
+void PrintString(char buffer[]);
 
 /* Address space control operations: Exit, Exec, Execv, and Join */
 
