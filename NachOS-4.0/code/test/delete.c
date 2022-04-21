4f10b8b90306b5fd3ddf8fe14bd77af750eb4e85
ngthtinh <nguyenthanhtinh365@gmail.com>
2022-04-22 01:48:30 +0700

    User program - delete: Xong!

--- NachOS-4.0/code/test/delete.c
+++ NachOS-4.0/code/test/delete.c
@@ -2,8 +2,28 @@
 
 #include "syscall.h"
 
+#define MAX_LENGTH 255
+
 int main()
 {
+    // Declaire
+    char filename[MAX_LENGTH];
+
+    // Read filename
+    PrintString("Enter file name to delete: ");
+    ReadString(filename, MAX_LENGTH);
+
+    if (Remove(filename) == 0)
+    {
+        PrintString("Delete file ");
+        PrintString(filename);
+        PrintString(" successfully!\n");
+    }
+    else
+    {
+        PrintString("Failed to delete file!\n");
+    }
+    
     Halt();
     /* not reached */
 }
