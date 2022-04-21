ce6cf964aa9743eacddc9a19658615a5bbbda716
ngthtinh <nguyenthanhtinh365@gmail.com>
2022-04-22 01:46:39 +0700

    User program - copy: Xong!

--- NachOS-4.0/code/test/copy.c
+++ NachOS-4.0/code/test/copy.c
@@ -6,6 +6,51 @@
 
 int main()
 {
+    // Declaire
+    int i, size, openFileId_1, openFileId_2;
+    char c, filename_1[MAX_LENGTH], filename_2[MAX_LENGTH];
+
+    // Read filename 1
+    PrintString("Enter source file name: ");
+    ReadString(filename_1, MAX_LENGTH);
+
+    // Open file
+    openFileId_1 = Open(filename_1);
+
+    // Display content
+    if (openFileId_1 >= 0)
+    {
+        // Read filename 2
+        PrintString("Enter destination file name: ");
+        ReadString(filename_2, MAX_LENGTH);
+
+        // Create file 2
+        Create(filename_2);
+
+        // Open file 2
+        openFileId_2 = Open(filename_2);
+
+        // Get file size
+        size = Seek(-1, openFileId_1);
+        Seek(0, openFileId_1);
+
+        // Print each byte to console
+        for (i = 0; i < size; i++)
+        {
+            Read(&c, 1, openFileId_1);
+            Write(&c, 1, openFileId_2);
+        }
+
+        // Close file
+        Close(openFileId_1);
+        Close(openFileId_2);
+
+        PrintString("Copy completed!\n");
+    }
+    else
+    {
+        PrintString("Cannot open source file!\n");
+    }
 
     Halt();
     /* not reached */
